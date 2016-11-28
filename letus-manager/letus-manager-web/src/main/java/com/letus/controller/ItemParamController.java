2528cdfa4bc7151089a54a64084c88c7a437b8e7
xfyin <xfyin@yinxuefei>
2016-11-29 00:31:29 +0800

    根据商品类目id 查询出商品规格模板 并将结果返回到页面 回调

---
+++ letus-manager/letus-manager-web/src/main/java/com/letus/controller/ItemParamController.java
@@ -0,0 +1,42 @@
+/**
+ * File：ItemParamController.java
+ * Package：com.letus.controller
+ * Author：xfyin
+ * Date：2016-11-29 上午12:05:48
+ * Copyright (C) 2016-2016 letus179-Copyright
+ */
+package com.letus.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.letus.common.pojo.LetusResult;
+import com.letus.service.ItemParamService;
+
+
+/**
+ * 商品规格参数模板管理
+ * 
+ * @author xfyin
+ *
+ */
+@Controller
+@RequestMapping(value="/item/param")
+public class ItemParamController {
+  
+  /**
+   * 注入service
+   */
+  @Autowired
+  private ItemParamService itemParamService;
+  
+  @RequestMapping(value="/query/itemcatid/{itemCatId}")
+  @ResponseBody
+  public LetusResult getItemParamByCid(@PathVariable long itemCatId) {
+    return itemParamService.getItemParamByCid(itemCatId);
+  }
+  
+}
