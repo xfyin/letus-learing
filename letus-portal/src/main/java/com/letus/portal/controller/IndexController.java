a383178017fb326cba6f8d89cceda5e1a9fc073c
xfyin <xfyin179@163.com>
2016-12-03 00:44:55 +0800

    jsonp跨域远程调用，商品页面分类展示 js

--- letus-portal/src/main/java/com/letus/portal/controller/IndexController.java
+++ letus-portal/src/main/java/com/letus/portal/controller/IndexController.java
@@ -10,16 +10,24 @@ package com.letus.portal.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 /**
  * 展示首页
  * 
  * @author xfyin
- *
+ * 
  */
 @Controller
 public class IndexController {
   
+  /**
+   * 页面URL：http://localhost:8082就能调用该方法的原因：
+   * 因为在web.xml中配置了欢迎页：index.html;即url相当于：
+   * http://localhost:8082/index.xml而在web.xml中
+   * 配置了请求的拦截规则*.html，springmvc会在controller中
+   * 找/index请求映射
+   * 
+   * @return 首页
+   */
   @RequestMapping("/index")
   public String showIndex() {
     return "index";
