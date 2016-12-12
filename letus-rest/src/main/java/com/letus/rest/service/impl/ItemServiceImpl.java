e7a5e8eb0c5ef8787360833549c97f899723876a
xfyin <xfyin179@163.com>
2016-12-12 22:59:45 +0800

    为查询商品基本信息添加缓存，存-取，缓存逻辑不能影响正常的业务

--- letus-rest/src/main/java/com/letus/rest/service/impl/ItemServiceImpl.java
+++ letus-rest/src/main/java/com/letus/rest/service/impl/ItemServiceImpl.java
@@ -7,11 +7,17 @@
  */
 package com.letus.rest.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonObjectFormatVisitor;
 import com.letus.common.pojo.LetusResult;
+import com.letus.common.utils.JsonUtils;
 import com.letus.mapper.TbItemMapper;
+import com.letus.pojo.TbItem;
+import com.letus.rest.dao.JedisClient;
 import com.letus.rest.service.ItemService;
 
 /**
@@ -24,14 +30,58 @@ import com.letus.rest.service.ItemService;
 public class ItemServiceImpl implements ItemService {
   
   /**
+   * 商品信息在redis中保存的key
+   */
+  @Value("{REDIS_ITEM_KEY}")
+  private String REDIS_ITEM_KEY;
+  
+  /**
+   * 商品过期时间
+   */
+  @Value("{REDIS_ITEM_EXPIRE}")
+  private Integer REDIS_ITEM_EXPIRE;
+  
+  /**
    * 注入mapper
    */
   @Autowired
   private TbItemMapper itemMapper;
   
+  /**
+   * 注入缓存
+   */
+  @Autowired
+  private JedisClient jedisClient;
+  
   @Override
   public LetusResult queryItemBaseInfo(long itemId) {
-    return LetusResult.ok(itemMapper.selectByPrimaryKey(itemId));
+    
+    String key = REDIS_ITEM_KEY + ":" + itemId + ":base";
+    
+    // 从缓存中取,r若存在直接返回
+    try {
+      String itemBaseInfo = jedisClient.get(key);
+      if (!StringUtils.isBlank(itemBaseInfo)) {
+        return LetusResult.ok(JsonUtils.jsonToPojo(itemBaseInfo, TbItem.class));
+      }
+    }
+    catch (Exception e1) {
+      e1.printStackTrace();
+    }
+    
+    // 根据商品id获取商品信息
+    TbItem item = itemMapper.selectByPrimaryKey(itemId);
+     
+    try {
+      // 把商品信息写入缓存，并设置key的有效期
+      jedisClient.set(key, JsonUtils.objectToJson(item));
+      jedisClient.expire(key, REDIS_ITEM_EXPIRE);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    
+    return LetusResult.ok(item);
   }
   
 }
